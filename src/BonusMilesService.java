6a890c6141b4ab19aac2d7550e3f319e03a496fd
lterentyeva <lterentyeva@yandex.ru>
2022-03-25 17:58:37 +0300

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        // Входные данные
+        int amount = 20;
+
+
+        // Логика программы
+        int bonus = cost / amount;
+        return bonus;
+    }
+}
+
